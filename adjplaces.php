b894298157af26b58d260592f9f1ffb45abeb660
Chris Clonch <chris@theclonchs.com>
2013-03-30 13:08:48 -0400

    Fixing comments for HTML

--- adjplaces.php
+++ adjplaces.php
@@ -1,4 +1,4 @@
-#
+<!--
 ## Copyright (c) 2013 <chris at theclonchs dot com>
 ##
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,6 +19,7 @@
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ## SOFTWARE.
 ##
+-->
 
 <html>
 <head></head>
